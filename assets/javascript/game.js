c35a0c65520619a135c990d2914024032619f66f
Wulfsbach <etemelkos@gmail.com>
2018-04-26 15:08:12 -0400

    Added set list of music/Sound Effects as well as Styling.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,203 @@
-//Variable Definitons
-var guessed = [];
-var guessright=[];
-const MaxTries = 10;
-var current[];
-var remainingGuesses= 0;
-var gamesStarted = false;
-var hasFinished= false;
-var wins=0;
-var spaceword = ["MassEffect", "Stellaris", "AlienIsolation","FasterThanLight", "NoMansSky","SurvivingMars", "Destiny", "Halo", "Starbound", "Spore", "SWOTR", "Prey", "DeadSpace","StarFox","Metroid"]
+//Create References
+var Save = document.getElementById('Save');
+var placeholder = document.getElementById('placeholders');
+var LettersGuessed = document.getElementById('guessed-letters');
+var RemainingGuesses = document.getElementById('Guesses-Left');
+var winner = document.getElementById('Wins');
+var loser = document.getElementById('Losses');
+var ship = document.getElementById('spaceship');
+var YouLose =document.getElementById("gameover");
+
+// create variables
+var spacewordbank = ["Mass Effect", "Stellaris", "Alien Isolation","Faster Than Light",
+ "No Mans Sky","Surviving Mars", "Destiny", "Halo", "Starbound", "Spore", "Star Wars The Old Republic", 
+ "Prey", "Dead Space","Star Fox","Metroid"];
+
+ var wins=0;
+ var losses=0;
+ var guessesLeft= 7;
+ var gameRunning=false;
+ var ChosenWord='';
+ var ChosenWordarr= [];
+ var GuessedLetters= [];
+ var IncorrectGuesses= [];
+ var pos=0;
+ 
+ //Function that resets and starts a new game.
+ document.getElementById("myAudio").volume = 0.2;
 
 
+ function start() {
 
+    gameRunning= true;
+    guessesLeft=7;
+    GuessedLetters= [];
+    IncorrectGuesses=[];
+    ChosenWordarr=[];
+    pos=0;
+    ship.style.left = pos;
+    YouLose.hidden=true;
+  planet.hidden=false;
+    ship.hidden=false;
+    YouLose.pause();
+     document.getElementById("myAudio").volume=0.2;
+   
 
-
-
-
-
-//Game-Reset
-function resetGame(){
-    remainingGuesses = MaxTries;
-    gamesStarted =false;
-    var word = spaceword[Math.floor(Math.random() * words.length)];
     
-guessed=[];
-guessright=[];
+    // Math to pick a word from array at random
 
-document.getElementById("hangmanalien").src="./assets/images/hangmanalien.jpeg"
-for (var i = 0; i < spaceword[current].length; i++) {
-    guessedright.push("_");
+    ChosenWord = spacewordbank[Math.floor(Math.random()* spacewordbank.length)];
+
+    // Creates Underscores in place of blanks
+
+    for (var i = 0; i < ChosenWord.length; i++){
+        if (ChosenWord[i] === ' '){
+            ChosenWordarr.push(' ');
+        }else{
+            ChosenWordarr.push('_');
+        }
+    }
+    //writes to html file
+    RemainingGuesses.innerHTML = guessesLeft;
+    placeholder.innerHTML = ChosenWordarr.join('');
+    LettersGuessed.innerHTML = IncorrectGuesses;
+ }
+
+ //Function to see if letter is in selected word
+ function Guess(letter) {
+console.log(letter);
+
+if (gameRunning && GuessedLetters.indexOf(letter) === -1) {
+
+//Run Game 
+GuessedLetters.push(letter);
+
+//check if guessed letter is in picked word
+
+for (var i = 0 ; i < ChosenWord.length ; i++) {
+    //convert both values to lower case for comaprison.
+    if (ChosenWord[i].toLowerCase() === letter.toLowerCase()) {
+        //if match swap out character in placeholder
+        ChosenWordarr[i] = ChosenWord[i];
+    }
+}
+placeholder.innerHTML = ChosenWordarr.join('');
+Incorrect(letter);
+}
+else{
+    if(!gameRunning) {
+        alert("Press the Button to save the world!")
+    } else {
+        alert("This button has already been pressed!")
+    }
+}
+ }
 
 
-    document.getElementById("pressKeyTryAgain").style.cssText= "display: none";
-    document.getElementById("gameover-image").style.cssText = "display: none";
-    document.getElementById("youwin-image").style.cssText = "display: none";
+ //Fuction to check for incorrect letters
 
-    updateDisplay();
+ function Incorrect(letter){
+     if (
+         ChosenWordarr.indexOf(letter.toLowerCase()) === -1 
+     && 
+        ChosenWordarr.indexOf(letter.toUpperCase()) === -1
+     ){
+         guessesLeft--;
+         IncorrectGuesses.push(letter);
+         LettersGuessed.innerHTML = IncorrectGuesses.join(' ');
+         RemainingGuesses.innerHTML = guessesLeft;
+        Destroy();
+     }
+  
+ Loss();
+GameOver();
+ }
+
+ function Loss (){
+if( guessesLeft === 0){
+    losses++;
+    gameRunning= false;
+    loser.innerHTML = losses;
+    document.getElementById("myAudio").volume=0;
+
+}
+Win();
+ }
+    
+
+function Win(){
+if( ChosenWord.toLowerCase() === ChosenWordarr.join('').toLowerCase()){
+    wins++;
+    gameRunning=false;
+    winner.innerHTML = wins;
+    document.getElementById("myAudio").volume=0;
+}
 }
 
-function updateDisplay(){
-
-    document.getElementById("totalWins").innerText = wins;
-    document.getElementById("currentWord").innerText="";
+function Destroy(){
     
-    for(var i=0; i< guessedright.length; i++){
+    if (guessesLeft == 6) {
+        pos++;
+        ship.style.left = pos + '2px';
 
-        document.getElementById("currentWord").innerText += guessedright[i];
     }
-    document.getElementById("remainingGuesses").innerText= remainingGuesses;
-    document.getElementById("guessedLetters").innerText= guessed;
-    if(guessed <= 0) {
-        document.getElementbyId("gameover-image").style.cssText= "display:block";
-        document.getElementbyId("pressKeyTryAgain").style.cssText="display:block";
+    if (guessesLeft == 5) {
+        pos++;
+        ship.style.left = pos + '10px';
+
     }
+    if (guessesLeft == 4) {
+        pos++;
+        ship.style.left = pos + '10px';
+
+    }
+    if (guessesLeft == 3) {
+        pos++;
+        ship.style.left = pos + '10px';
+
+    }
+    if (guessesLeft == 2) {
+        pos++;
+        ship.style.left = pos + '20px';
+
+    }
+    if (guessesLeft == 1) {
+        pos++;
+        ship.style.left = pos + '20px';
+
+    }
+    if (guessesLeft == 0) {
+        pos++;
+        ship.style.left = pos + '40px';
+
+    }
+}
+
+function GameOver() {
+
+
+if (guessesLeft === 0 && !gameRunning){
+    YouLose.hidden=false;
+    YouLose.load();
+    YouLose.play();
+    planet.hidden=true;
+    ship.hidden=true;
+}
+
 }
 
 
 
+ //button
+ 
+ Save.addEventListener('click', start,);
 
+ //on key up event
 
-
-
-
-
-
-    
+ document.onkeyup = function (event) {
+     console.log(event);
+     if(event.keyCode >=65 && event.keyCode <=90) {
+         Guess(event.key);
+         
+     }
+ }
